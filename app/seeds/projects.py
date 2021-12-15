af2599e8ca5a4f04e16419bc70e6a3cdba21ea1e
Rob Strasser <roestrasser@sbcglobal.net>
2021-12-15 15:32:30 -0800

    projects seeded

--- app/seeds/projects.py
+++ app/seeds/projects.py
@@ -5,26 +5,27 @@ def seed_projects():
         user_id=1,
         category_id=1,
         title='Change your oil in the garage',
-        content='After 30 years of changing the oil of my cars, I\'ve decided to share my knowledge with you all'
+        content='''After 30 years of changing the oil of my cars,
+            I\'ve decided to share my knowledge with you all'''
     )
 
     project2 = Project(
         user_id=1,
-        category_id=1,
-        title='',
-        content=''
+        category_id=5,
+        title='Bird Feeder',
+        content='I have the perfect solution to your bird feeder issues'
     )
 
     project3 = Project(
-        user_id=1,
-        category_id=1,
-        title='',
-        content=''
+        user_id=2,
+        category_id=3,
+        title='Clean those dirty windows with ease',
+        content='The one method Windex doesn\'t want you to know!'
     )
 
     project4 = Project(
         user_id=1,
-        category_id=1,
-        title='',
-        content=''
+        category_id=6,
+        title='Spaghetti',
+        content='Better than yo mama\'s'
     )
