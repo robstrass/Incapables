30a7f241604b6fb6cffa71eb5d135ce367ab4872
Rob Strasser <roestrasser@sbcglobal.net>
2021-12-16 21:45:59 -0800

    delete comment added and tested

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -11,3 +11,13 @@ def all_comments(commentId):
     comment = Comment.query.get(int(commentId))
 
     return comment.to_dict()
+
+@comment_routes.route('/<int:commentId>', methods=['DELETE'])
+# @login_required
+def delete_comment(commentId):
+    comment = Comment.query.get(int(commentId))
+    if comment.user_id == 2: #current_user.id:
+        db.session.delete(comment)
+        db.session.commit()
+        return comment.to_dict()
+    return {'errors': 'You can\'t delete someone else\'s comment.'}
