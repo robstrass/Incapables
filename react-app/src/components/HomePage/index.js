04d54c7dca96e246b553bc5e4744ba64f1e7d9db
Rob Strasser <roestrasser@sbcglobal.net>
2021-12-17 23:22:49 -0800

    added content div for main page

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import './HomePage.module.css';
+import style from './HomePage.module.css';
 
 export default function HomePage() {
     const user = useSelector(state => state.session.user)
 
     return (
-        <div>hello</div>
+        <div className={style.homeContainer}>
+            <div className={style.homeFeatured}>
+                Featured Projects
+            </div>
+            <div className={style.homeInfoContainer}>
+                <div className={style.homeInfoSingleDiv}>
+                    <h2 className={style.homeInfoTitle}>
+                        STEP-BY-STEP
+                    </h2>
+                    <p className={style.homeInfoContent}>
+                        Content
+                    </p>
+                </div>
+                <div className={style.homeInfoSingleDiv}>
+                    <h2 className={style.homeInfoTitle}>
+                        CURATED BY YOUR PEERS
+                    </h2>
+                    <p className={style.homeInfoContent}>
+                        Content
+                    </p>
+                </div>
+                <div className={style.homeInfoSingleDiv}>
+                    <h2 className={style.homeInfoTitle}>
+                        FAMILY FRIENDLY
+                    </h2>
+                    <p className={style.homeInfoContent}>
+                        Content
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
