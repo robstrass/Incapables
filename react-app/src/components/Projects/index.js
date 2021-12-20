51b924eb273dc34c2504be13c1d98e3970e6858c
Rob Strasser <roestrasser@sbcglobal.net>
2021-12-19 22:36:58 -0800

    started store for projects

---
+++ react-app/src/components/Projects/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+
+import style from './Projects.module.css';
+
+export default function Projects() {
+    const dispatch = useDispatch();
+}
