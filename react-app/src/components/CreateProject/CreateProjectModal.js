010af8137477a6da2f2efe1fb46b7b0ec098b74f
Rob Strasser <roestrasser@sbcglobal.net>
2021-12-21 22:15:22 -0800

    form done, needs styling

--- react-app/src/components/CreateProject/CreateProjectModal.js
+++ react-app/src/components/CreateProject/CreateProjectModal.js
@@ -7,10 +7,16 @@ import { allCategoriesThunk } from '../../store/categories';
 
 export default function CreateProjectModal({ setCreateProject }) {
     const dispatch = useDispatch();
+    const allCategories = useSelector(state => Object.values(state.categories.all));
+    console.log('create proj categories', allCategories);
 
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [categoryId, setCategoryId] = useState('');
+
+    useEffect(() => {
+        dispatch(allCategoriesThunk());
+    }, [dispatch]);
 
     return (
         <>
@@ -55,8 +61,35 @@ export default function CreateProjectModal({ setCreateProject }) {
                         />
                     </div>
                     <div className={style.createProjectModalDiv}>
+                        <label
+                            className={style.createProjectModalLabel}
+                            htmlFor='projectSelect'
+                        >
+                            Select a category for your project:
+                        </label>
                         <select
-                            value=
+                            id='projectSelect'
+                            className={style.createProjectModalSelect}
+                            value={categoryId}
+                            onChange={(e) => setCategoryId(e.target.value)}
+                        >
+                            {allCategories?.map(category => (
+                                <option
+                                    value={category.id}
+                                    key={category.id}
+                                >
+                                    {category.category}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className={style.createProjectModalDiv}>
+                        <button
+                            className={style.createProjectSubmit}
+                            onClick={() => setCreateProject(false)}
+                        >
+                            Start Incapable
+                        </button>
                     </div>
                 </form>
             </div>
