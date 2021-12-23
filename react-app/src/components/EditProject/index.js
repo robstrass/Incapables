e94a6dc2c0ae5280489d9f8c42d1c91a2085baa9
Rob Strasser <roestrasser@sbcglobal.net>
2021-12-22 22:46:02 -0800

    edit project modal structured, needs styling

---
+++ react-app/src/components/EditProject/index.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import style from './EditProject.module.css';
+import header from '../../assets/Instructables-Create-Header.png';
+import { allCategoriesThunk } from '../../store/categories';
+
+export default function EditProject({ setEditModal }) {
+    const dispatch = useDispatch();
+    const project = useSelector(state => state.projects.current);
+    const allCategories = useSelector(state => Object.values(state.categories.all));
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [categoryId, setCategoryId] = useState(1);
+    const [errors, setErrors] = useState([]);
+
+    const validate = () => {
+        const validation = [];
+        if (!title) validation.push('Please name your project.');
+        if (!content) validation.push('Please provide a description.');
+        if (!categoryId) validation.push('Please choose a category.');
+
+        return validation;
+    }
+
+    useEffect(() => {
+        dispatch(allCategoriesThunk());
+    }, [dispatch]);
+
+    return (
+        <>
+            <div
+                className={style.editProjbackground}
+                onClick={() => setEditModal(false)}
+            ></div>
+            <div className={style.editProjContainer}>
+                <img
+                    className={style.editProjImg}
+                    src={header}
+                />
+                <form
+                    className={style.editProjForm}
+                >
+                    <div className={style.editProjDiv}>
+                        <label
+                            className={style.editProjLabel}
+                            htmlFor='editProjTitle'
+                        >
+                            I made a project called:
+                            <span className={style.editProjErrors}>
+                                {errors.length > 0 && errors.map(error => (
+                                    error.includes('project')
+                                )) ? errors.map(error => error.includes('project') ?
+                                `${error}` : null) : null}
+                            </span>
+                        </label>
+                        <input
+                            className={style.editProjInput}
+                            id='editProjTitle'
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
+                    </div>
+                    <div className={style.editProjDiv}>
+                        <label
+                            className={style.editProjLabel}
+                            htmlFor='projectContent'
+                        >
+                            Project description:
+                            <span className={style.editProjErrors}>
+                                {errors.length > 0 && errors.map(error => (
+                                    error.includes('description')
+                                )) ? errors.map(error => error.includes('description') ?
+                                `${error}` : null) : null}
+                            </span>
+                        </label>
+                        <textarea
+                            className={style.editProjTextArea}
+                            id='projectContent'
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                        />
+                    </div>
+                    <div className={style.editProjDiv}>
+                        <label
+                            className={style.editProjLabel}
+                            htmlFor='projectSelect'
+                        >
+                            Select a category for your project:
+                            <span className={style.editProjErrors}>
+                                {errors.length > 0 && errors.map(error => (
+                                    error.includes('category')
+                                )) ? errors.map(error => error.includes('category') ?
+                                `${error}` : null) : null}
+                            </span>
+                        </label>
+                        <select
+                            id='projectSelect'
+                            className={style.editProjSelect}
+                            value={categoryId}
+                            onChange={(e) => setCategoryId(e.target.value)}
+                        >
+                            {allCategories?.map(category => (
+                                <option
+                                    value={category.id}
+                                    key={category.id}
+                                >
+                                    {category.category}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <button
+                        className={style.editProjSubmit}
+                        type='submit'
+                    >
+                        Edit Incapable
+                    </button>
+                </form>
+            </div>
+        </>
+    )
+}
--- react-app/src/components/SingleProject/index.js
+++ react-app/src/components/SingleProject/index.js
@@ -4,6 +4,7 @@ import { NavLink, useParams } from 'react-router-dom';
 
 import style from './SingleProject.module.css';
 import * as projectsActions from '../../store/projects';
+import EditProject from '../EditProject';
 
 export default function SingleProject() {
     const dispatch = useDispatch();
@@ -24,48 +25,60 @@ export default function SingleProject() {
     }, [dispatch]);
 
     return (
-        <div className={style.singleProjContainer}>
-            <div className={style.singleProjHolder}>
-                <h1 className={style.singleProjTitle}>
-                    {project ? project.title : null}
-                </h1>
-                <p className={style.singleProjAuthor}>
-                    by {project ? project.author?.username : null}
-                </p>
-                <p className={style.singleProjContent}>
-                    {project ? project.content : null}
-                </p>
-                { ownerVerify && (
-                    <div className={style.singleProjOwnerButtons}>
-                        <div
-                            className={style.singleProjEdit}
-                            onClick={() => setEditModal(true)}
-                        >
-                            Edit
+        <>
+            {editModal && (
+                <EditProject
+                    setEditModal={setEditModal}
+                />
+            )}
+            <div className={style.singleProjContainer}>
+                <div className={style.singleProjHolder}>
+                    <h1 className={style.singleProjTitle}>
+                        {project ? project.title : null}
+                    </h1>
+                    <p className={style.singleProjAuthor}>
+                        by {project ? project.author?.username : null}
+                    </p>
+                    <p className={style.singleProjContent}>
+                        {project ? project.content : null}
+                    </p>
+                    { ownerVerify && (
+                        <div className={style.singleProjOwnerButtons}>
+                            <div
+                                className={style.singleProjEdit}
+                                onClick={() => setEditModal(true)}
+                            >
+                                Edit
+                            </div>
+                            <div
+                                className={style.singleProjDelete}
+                                onClick={() => setDeleteModal(true)}
+                            >
+                                Delete
+                            </div>
                         </div>
-                        <div className={style.singleProjDelete}>Delete</div>
-                    </div>
-                )}
+                    )}
+                </div>
+                <div className={style.singleProjSteps}>
+                    { project ? project.images?.map((image, index) => (
+                        <div
+                            key={image.id}
+                            className={style.singleProjSingleStep}
+                        >
+                            <img
+                                className={style.singleProjImage}
+                                src={image.image}
+                            />
+                            <h2 className={style.singleProjStepCount}>
+                                Step #{index + 1}
+                            </h2>
+                            <p className={style.singleProjStepContent}>
+                                {image.content}
+                            </p>
+                        </div>
+                    )) : null}
+                </div>
             </div>
-            <div className={style.singleProjSteps}>
-                { project ? project.images?.map((image, index) => (
-                    <div
-                        key={image.id}
-                        className={style.singleProjSingleStep}
-                    >
-                        <img
-                            className={style.singleProjImage}
-                            src={image.image}
-                        />
-                        <h2 className={style.singleProjStepCount}>
-                            Step #{index + 1}
-                        </h2>
-                        <p className={style.singleProjStepContent}>
-                            {image.content}
-                        </p>
-                    </div>
-                )) : null}
-            </div>
-        </div>
+        </>
     )
 }
