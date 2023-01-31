d3a0470b5398acce549aadf8828e7f757ee5d375
Rob Strasser <roestrasser@sbcglobal.net>
2023-01-30 22:25:03 -0800

    structure and styling for about us video

--- react-app/src/components/HomePage/AboutUs.js
+++ react-app/src/components/HomePage/AboutUs.js
@@ -6,15 +6,17 @@ export default function AboutUs() {
     return (
         <div className={style.aboutUsContainer}>
             <div className={style.aboutUsWrapper}>
-                <h2 className={style.aboutUsTitle}>INCAPABLES IS A PLACE THAT LETS YOU EXPLORE, DOCUMENT, AND SHARE YOUR CREATIONS.</h2>
-                <div className={style.aboutUsVideoContainer}>
-                    <iframe
-                        width="776" height="436"
-                        src="https://www.youtube.com/embed/k12h_FOInZg"
-                        title="Instructables - Explore. Share. Make." frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen>
-                    </iframe>
+                <div className={style.aboutUsTitleWrapper}>
+                    <h2 className={style.aboutUsTitle}>INCAPABLES IS A PLACE THAT LETS YOU EXPLORE, DOCUMENT, AND SHARE YOUR CREATIONS.</h2>
+                    <div className={style.aboutUsVideoContainer}>
+                        <iframe
+                            width="776" height="436"
+                            src="https://www.youtube.com/embed/k12h_FOInZg"
+                            title="Instructables - Explore. Share. Make." frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen>
+                        </iframe>
+                    </div>
                 </div>
                 <div className={style.aboutUsStoryWrapper}>
                     <h3>Our Story</h3>
