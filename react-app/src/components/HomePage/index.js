812cbdaa5b9840ba91b8582f3f2655369e01885f
Rob Strasser <robstrasserdev@gmail.com>
2024-03-20 19:01:31 -0700

    refactored the entire categories section to be dynamic and more readable

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -6,7 +6,7 @@ import style from "./HomePage.module.css";
 
 import FeaturedProject from "./FeaturedProject";
 import HomePageInfo from "./HomePageInfo";
-import ProjectSection from "./ProjectSection";
+import ProjectSectionHeader from "./ProjectSectionHeader";
 import Footer from "../Footer";
 import { COPY } from "../../shared/copyConstants";
 
@@ -19,13 +19,12 @@ export default function HomePage() {
   const projectsObject = useSelector(state => state.projects.all);
   const projects = Object.values(projectsObject);
   const projectKeys = Object.keys(projectsObject);
+  const categories = Object.values(allCategories);
 
   useEffect(() => {
     dispatch(categoriesActions.allCategoriesThunk());
     dispatch(projectActions.allProjectsThunk());
   }, [dispatch]);
-
-  console.log("allCategories: ", allCategories);
 
   return (
     <div className={style.homeContainer}>
@@ -38,97 +37,10 @@ export default function HomePage() {
       </div>
       <div className={style.homeCategoriesContainer}>
         <h2 className={style.homeCategoriesTitle}>{COPY.EXPLORE_PROJECTS}</h2>
-        <div className={style.homeCategoriesDiv}>
-          <NavLink
-            className={style.homeSingleCategoryTitle}
-            to={"/categories/1"}
-          >
-            Workshop<span className="material-icons">chevron_right</span>
-          </NavLink>
-          <div className={style.homeCategoryProjectsHolder}>
-            {allCategories ? (
-              <ProjectSection projects={allCategories[1]?.projects} />
-            ) : null}
-          </div>
-        </div>
-        <div className={style.homeCategoriesDiv}>
-          <NavLink
-            className={style.homeSingleCategoryTitle}
-            to={"/categories/2"}
-          >
-            Gardening<span className="material-icons">chevron_right</span>
-          </NavLink>
-          <div className={style.homeCategoryProjectsHolder}>
-            {allCategories ? (
-              <ProjectSection projects={allCategories[2]?.projects} />
-            ) : null}
-          </div>
-        </div>
-        <div className={style.homeCategoriesDiv}>
-          <NavLink
-            className={style.homeSingleCategoryTitle}
-            to={"/categories/3"}
-          >
-            Living<span className="material-icons">chevron_right</span>
-          </NavLink>
-          <div className={style.homeCategoryProjectsHolder}>
-            {allCategories
-              ? <ProjectSection projects={allCategories[3]?.projects} />
-              : null}
-          </div>
-        </div>
-        <div className={style.homeCategoriesDiv}>
-          <NavLink
-            className={style.homeSingleCategoryTitle}
-            to={"/categories/4"}
-          >
-            Outside<span className="material-icons">chevron_right</span>
-          </NavLink>
-          <div className={style.homeCategoryProjectsHolder}>
-            {allCategories
-              ? <ProjectSection projects={allCategories[4]?.projects} />
-              : null}
-          </div>
-        </div>
-        <div className={style.homeCategoriesDiv}>
-          <NavLink
-            className={style.homeSingleCategoryTitle}
-            to={"/categories/5"}
-          >
-            Craft<span className="material-icons">chevron_right</span>
-          </NavLink>
-          <div className={style.homeCategoryProjectsHolder}>
-            {allCategories
-              ? <ProjectSection projects={allCategories[5]?.projects} />
-              : null}
-          </div>
-        </div>
-        <div className={style.homeCategoriesDiv}>
-          <NavLink
-            className={style.homeSingleCategoryTitle}
-            to={"/categories/6"}
-          >
-            Cooking<span className="material-icons">chevron_right</span>
-          </NavLink>
-          <div className={style.homeCategoryProjectsHolder}>
-            {allCategories
-              ? <ProjectSection projects={allCategories[6]?.projects} />
-              : null}
-          </div>
-        </div>
-        <div className={style.homeCategoriesDiv}>
-          <NavLink
-            className={style.homeSingleCategoryTitle}
-            to={"/categories/7"}
-          >
-            Miscellaneous<span className="material-icons">chevron_right</span>
-          </NavLink>
-          <div className={style.homeCategoryProjectsHolder}>
-            {allCategories
-              ? <ProjectSection projects={allCategories[7]?.projects} />
-              : null}
-          </div>
-        </div>
+        {categories &&
+          categories.map((category, idx) => (
+            <ProjectSectionHeader category={category} keyVal={idx} />
+          ))}
       </div>
       <Footer />
     </div>
