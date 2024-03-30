c341b01d223cc57e2035e4209ebc285b8c302e7f
Rob Strasser <robstrasserdev@gmail.com>
2024-03-29 20:31:42 -0700

    added constants to about us page

--- react-app/src/shared/copyConstants.js
+++ react-app/src/shared/copyConstants.js
@@ -9,4 +9,47 @@ export const COPY = {
     "Incapables are created by you, the community. We all have skills and knowledge, but no one wants to share that. Join the community today to share how NOT to complete your project.",
   HOMEPAGE_3:
     "Sarcasm is one of our core principles. This community is meant to be light-hearted and all in good taste. Find your happy place here to fulfill all your satirical needs.",
+  HELP_CENTER: "Help Center",
+  HELP_CENTER_PARAGRAPH: `Writing a good incapable is a lot easier than you think! Take
+  a look at some of the incapables already written on the site
+  to get a feel for how things are written. Those will give you
+  a good idea for how to create a good incapable.
+
+  With so many incapables being created on the site all the time,
+  you should have plenty of ideas on what to do to get started on
+  your very own incapable.`,
+  HOW_TO_WRITE_INCAPABLE: "How to Write an Incapable",
+  HOW_TO_WRITE_INCAPABLE_PARAGRAPH_1: `To start a new Incapable, go to the Publish button in the main
+  navigation bar at the top of the site. Once there, click "Log In"
+  or "Sign Up" if you don't have an account. You need to be a member of Incapables to author your own
+  Incapable. If you aren't already a member, feel free to sign up
+  and join the community. It's 100% free to join`,
+  HOW_TO_WRITE_INCAPABLE_PARAGRAPH_2: `Once you begin your Incapable, the first step is to name it.
+  You'll want your name to be simple, clear, and descriptive so
+  others will be able to identify exactly what your Incapable is
+  about. Next, you'll want to give your incapable a brief description so
+  community members can see what your project is about. This will
+  help others get an idea of what your incapable does. Finally, you'll want to select a topic category for your
+  incapable. Categories help other members of the community find
+  incapables that they're interested in. Each project can only have
+  one category, so choose the category that best fits your
+  incapable.`,
+  HOW_TO_WRITE_INCAPABLE_PARAGRAPH_3: `After you've finished with those first steps, just press "Start
+  Incapable", and you'll be well on your way to creating your first
+  Incapable! Congratulations, and welcome to the Incapables
+  community.`,
+  ABOUT_US: "INCAPABLES IS A PLACE THAT LETS YOU EXPLORE, DOCUMENT, AND SHARE YOUR CREATIONS.",
+  OUR_STORY: "Our Story",
+  OUR_STORY_PARAGRAPH: `Incapables was created by Rob Strasser as a spinoff of the popular DIY website
+  Instructables, where users can create step by step guides for activities, crafts,
+  hobbies, etc. Instead of making a direct clone, Rob wanted to key into his sarcastic
+  sense of humor and make this the opposite -- instead of helpful DIY guides,
+  Incapables serves as an anti guide, purposely, sometimes even hyperbolically,
+  incorrect instructions for said activities.
+
+  Incapables served as Rob's capstone project, marking the completion of his 6-month immersive
+  web development bootcamp. In this fullstack project, Rob utilized all the skills and knowledge
+  he picked up, both from working on other solo projects, and collaborating with colleagues.
+  This project utilizes a React and JavaScript front end, with Redux for state storage, and a
+  backend written in Python, with Flask, SQLAlchemy, and Amazon's S3 buckets for image storage.`
 };
